c2b9f12149d8c8e901216cb715fc5967794c1615
shaniAbeywick <shaniabeywickrama294@gmail.com>
2022-06-17 21:03:22 +0530

    complete simple interest calculator using pure HTML,CSS and Javascript

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
+   var amount = parseInt(document.getElementById("principal").value);
+   var rate = parseInt(document.getElementById("vol").value);
+   var time = parseInt(document.getElementById("years").value);
+
+   var interest = amount* rate * time /100 ;
+   var total = interest + amount;
+
+   document.getElementById("output_1").innerHTML="If you deposit " + amount;
+   document.getElementById("output_2").innerHTML="at an interest rate of " + rate +"%";
+   document.getElementById("output_3").innerHTML="You will receive an amount of " + total;
+   document.getElementById("output_4").innerHTML="in the year " + time;
+
+
+
+   
+
+
+   
+
+   
+   
+
+
     
 }
         
